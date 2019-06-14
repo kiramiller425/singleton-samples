ae6466f7d89dbf5d76a64b35251b7b574dd8d46e
Kira Miller <kira.miller2011@gmail.com>
2019-06-14 14:34:18 -0700

    started project with single example

---
+++ SingletonSamples/NonThreadSafeSingleton.cs
@@ -0,0 +1,36 @@
+﻿
+namespace SingletonSamples
+{
+    /// <summary>
+    /// This one is not thread-safe and not a valid Singleton as at least 2 threads could potentially 
+    /// evaluate that if statement at nearly the same time and create multiple instances, which 
+    /// invalidates this pattern.
+    /// </summary>
+    public sealed class NonThreadSafeSingleton
+    {
+        private static NonThreadSafeSingleton _instance = null;
+
+        private NonThreadSafeSingleton()
+        {
+
+        }
+
+        public static NonThreadSafeSingleton Instance
+        {
+            get
+            {
+                if(_instance == null)
+                {
+                    _instance = new NonThreadSafeSingleton();
+                }
+                else
+                {
+                    // this is just to demonstrate
+                    throw new System.Exception("Instance already exists.");
+                }
+
+                return _instance;
+            }
+        }
+    }
+}
---
+++ SingletonSamples/Program.cs
@@ -0,0 +1,53 @@
+﻿/* Singleton Samples
+ * Author: Kira Miller
+ * Notes: This is just some practice as I follow along with https://csharpindepth.com/articles/singleton
+ */
+
+using System;
+using System.Threading;
+
+namespace SingletonSamples
+{
+    public class Program
+    {
+        public static NonThreadSafeSingleton nonThreadSafeSingleton1;
+        public static NonThreadSafeSingleton nonThreadSafeSingleton2;
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Starting samples...");
+
+            #region NonThreadSafeSingleton
+
+            try
+            {
+                // In a valid Singleton pattern, these should throw an exception:
+                ThreadPool.QueueUserWorkItem(SetupNonThreadSafeSingleton1);
+                ThreadPool.QueueUserWorkItem(SetupNonThreadSafeSingleton2);
+            }
+            catch(Exception exception)
+            {
+                Console.WriteLine("It will not get here. " + exception.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Completed example of NonThreadSafeSingleton.");
+            }
+
+            #endregion
+
+
+            Console.WriteLine("Hello World!");
+            var m = Console.ReadLine();
+        }
+
+        public static void SetupNonThreadSafeSingleton1(Object stateInfo)
+        {
+            nonThreadSafeSingleton1 = NonThreadSafeSingleton.Instance;
+        }
+        public static void SetupNonThreadSafeSingleton2(Object stateInfo)
+        {
+            nonThreadSafeSingleton2 = NonThreadSafeSingleton.Instance;
+        }
+    }
+}
